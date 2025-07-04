fd5058a75410e4475df4d8cc55a72e6056764f22
Amr <amrkhaled12338@gmail.com>
2025-07-04 10:30:39 +0300

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
-import Menu from "./components/menu";
+import Menu from "./components/Menu";
 import Contact from "./components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
